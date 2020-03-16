f98f00fc5f8afb3bff67b7e88e4c61804a8d22c1
dawnight <weizhiqimail@gmail.com>
2020-03-16 22:24:57 +0800

    修改 README

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -90,10 +90,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    enableTracing: true,
-    initialNavigation: 'enabled'
-})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
